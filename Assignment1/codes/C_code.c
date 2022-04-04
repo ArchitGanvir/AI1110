dd74be33b4ff84d4da054756587bce2ead58260d
Archit Ganvir <96365051+ArchitGanvir@users.noreply.github.com>
2022-04-04 07:09:37 +0530

    Add files via upload

---
+++ Assignment1/codes/C_code.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int recdep(int P, int n, int r)
+{
+  int MV;
+  MV = P * n + P * (n * (n + 1) / (2 * 12.0)) * (r / 100.0);
+  return MV;
+}
+
+int main()
+{
+  int P, n, r, MV;
+  P = 600;
+  n = 2.5 * 12;
+  r = 10;
+  MV = recdep(P, n, r);
+  printf("The maturity value of the recurring deposit is %d\n", MV);
+
+  return 0;
+}
